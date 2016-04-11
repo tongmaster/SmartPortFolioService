ed952e80a60f4d2e2efe834a6cf857086b3742d6
tongmaster <basfers@gmail.com>
2016-04-11 16:54:40 +0700

    add enroll

---
+++ src/com/portfolio/service/EnrollEndpoint.java
@@ -0,0 +1,71 @@
+package com.portfolio.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+@Path("/test")
+public class EnrollEndpoint {
+	
+	@Path("/xml")
+	@GET
+	@Produces("application/xml")
+	public String convertCtoF() {
+ 
+		Double fahrenheit;
+		Double celsius = 36.8;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+ 
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+ 
+	@Path("xml/{c}")
+	@GET
+	@Produces("application/xml")
+	public String convertCtoFfromInput(@PathParam("c") Double c) {
+		Double fahrenheit;
+		Double celsius = c;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+ 
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+	
+	@Path("/json")
+	@GET
+	  @Produces("application/json")
+	  public Response convertFtoC() throws JSONException {
+
+		JSONObject jsonObject = new JSONObject();
+		Double fahrenheit = 98.24;
+		Double celsius;
+		celsius = (fahrenheit - 32)*5/9; 
+		jsonObject.put("F Value", fahrenheit); 
+		jsonObject.put("C Value", celsius);
+
+		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		return Response.status(200).entity(result).build();
+	  }
+
+	  @Path("json/{f}")
+	  @GET
+	  @Produces("application/json")
+	  public Response convertFtoCfromInput(@PathParam("f") float f) throws JSONException {
+
+		JSONObject jsonObject = new JSONObject();
+		float celsius;
+		celsius =  (f - 32)*5/9; 
+		jsonObject.put("F Value", f); 
+		jsonObject.put("C Value", celsius);
+
+		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		return Response.status(200).entity(result).build();
+	  }
+
+}
